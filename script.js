fb0648b98c67004cec1e31ce13cd5fe15012e8b3
vn <vnascimento001@hotmail.com>
2021-03-28 09:29:45 -0300

    funções

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function Get(url){
     return request.responseText
 }
 
+function multiplica(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
 function main() {
   //console.log(Get("https://economia.awesomeapi.com.br/all/USD-BRL"))
   valor = Get("https://economia.awesomeapi.com.br/all/USD-BRL,EUR-BRL")
@@ -29,17 +37,17 @@ function main() {
 
   //condicionais
   if (de.value == "EUR" && para.value == "BRL" && valorInput != null ){
-      valorFinal.innerHTML = "R$" + (valorInput * eur).toFixed(2);
+      valorFinal.innerHTML = "R$" + multiplica(valorInput,eur).toFixed(2);
   }else if (de.value == "USD" && para.value == "BRL" && valorInput != null){
-    valorFinal.innerHTML = "R$" + (valorInput * usd).toFixed(2);
+    valorFinal.innerHTML = "R$" + multiplica(valorInput, usd).toFixed(2);
   }else if (de.value == "BRL" && para.value == "EUR" && valorInput != null){
-    valorFinal.innerHTML = "€" + (valorInput / eur).toFixed(2);
+    valorFinal.innerHTML = "€" + divide(valorInput, eur).toFixed(2);
   }else if (de.value == "BRL" && para.value == "USD" && valorInput != null){
-    valorFinal.innerHTML = "US$" + (valorInput / usd).toFixed(2);
+    valorFinal.innerHTML = "US$" + divide(valorInput, usd).toFixed(2);
   }else if (de.value == "USD" && para.value == "EUR" && valorInput != null){
-    valorFinal.innerHTML = "€" + (valorInput / eurusd).toFixed(2);
+    valorFinal.innerHTML = "€" + divide(valorInput, eurusd).toFixed(2);
   }else if (de.value == "EUR" && para.value == "USD" && valorInput != null){
-    valorFinal.innerHTML = "US$" + (valorInput * eurusd).toFixed(2);
+    valorFinal.innerHTML = "US$" + multiplica(valorInput, eurusd).toFixed(2);
   }else if (de.value == "BRL" && para.value == "BRL" && valorInput != null){
     valorFinal.innerHTML = "R$" + valorInput;
   }else if (de.value == "USD" && para.value == "USD" && valorInput != null){
